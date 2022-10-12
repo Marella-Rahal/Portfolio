7c10db44c2fc197bbb7f77bb28de699de6961bed
Marella-Rahal <marellarahhal20@gmail.com>
2022-10-12 18:40:39 +0300

    Portfolio

--- components/Navbar.js
+++ components/Navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
 
             <div className={nav?'fixed w-full h-screen bg-black/50 md:hidden':'hidden'}>
 
-                    <div className='w-[75%] md:w-[60%] lg:w-[45%] h-screen bg-[#ecf0f3] flex flex-col p-10 space-y-8'>
+                    <div className='w-[75%] md:w-[60%] lg:w-[45%] h-screen bg-[#ecf0f3] flex flex-col justify-between p-7'>
 
 
 
@@ -91,11 +91,11 @@ const Navbar = () => {
 
 
 
-                        <div className='fixed bottom-5 flex flex-col space-y-5'>
+                        <div className='flex flex-col space-y-5'>
 
                             <p className='text-[#5651e5] uppercase'>Let&apos;s Connect</p>
 
-                            <div className='-ml-8 xs:ml-0 flex space-x-2'>
+                            <div className='-ml-6 xs:ml-0 flex space-x-2'>
 
                                 <div className='hover:scale-[1.1] rounded-full shadow-lg p-3'>
                                     <FaLinkedinIn size={'20px'}/>
