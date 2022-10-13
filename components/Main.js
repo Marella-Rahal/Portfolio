92a000d39519409994efa52ffc6c04362b39caa3
Marella-Rahal <marellarahhal20@gmail.com>
2022-10-13 10:20:56 +0300

    Portfolio

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import {AiOutlineMail} from 'react-icons/ai';
+import {FaLinkedinIn} from 'react-icons/fa';
+import {BsGithub,BsFillPersonLinesFill} from 'react-icons/bs';
 
 const Main = () => {
   return (
-    <div>
+    <div className='pt-52 space-y-3 text-center'>
+
+        <p>
+            Let&apos;s build somthing together
+        </p>
+        <h1>
+            Hi,I&apos;m  
+            <span className='text-effectColor'> Marella</span>
+        </h1>
+        <h1>
+            A Front-End Web Developer
+        </h1>
+        <p className='max-w-xl m-auto'>
+            I&apos;m a front-end web developer specializing in building exceptional digital experiences. Currently, I&apos;m focused on building responsive front-end web applications while learning the deep learning technique
+        </p>
+
+        <div className='flex justify-center space-x-5 pt-3'>
+
+            <div className='hover:scale-[1.1] rounded-full shadow-lg shadow-shadowColor p-3'>
+                <FaLinkedinIn size={'20px'}/>
+            </div>
+
+            <div className='hover:scale-[1.1] rounded-full shadow-lg shadow-shadowColor p-3'>
+                <BsGithub size={'20px'}/>
+            </div>
+
+            <div className='hover:scale-[1.1] rounded-full shadow-lg shadow-shadowColor p-3'>
+                <AiOutlineMail size={'20px'}/>
+            </div>
+
+            <div className='hover:scale-[1.1] rounded-full shadow-lg shadow-shadowColor p-3'>
+                <BsFillPersonLinesFill size={'20px'}/>
+            </div>
+
+        </div>
       
     </div>
   )
