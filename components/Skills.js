e637e44beaafe12a5febfa252eddb8d55c6a33c5
Marella-Rahal <marellarahhal20@gmail.com>
2022-10-13 18:00:22 +0300

    Portfolio

--- components/Skills.js
+++ components/Skills.js
@@ -6,57 +6,57 @@ const Skills = () => {
     <div className='h-screen px-10 py-20 mt-48 xs:mt-0 space-y-7'>
       <p className='text-effectColor tracking-widest uppercase'>skills</p>
       <h2>What I Can Do</h2>
-      <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 
         {/* html */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/html.png' alt="" width={'100'} height={'100'}/>
-            <h2>Html</h2>
+            <Image src='/../public/html.png' alt="" width={'75'} height={'75'}/>
+            <h3>Html</h3>
         </div>
 
         {/* css */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/css.png' alt="" width={'100'} height={'100'}/>
-            <h2>CSS</h2>
+            <Image src='/../public/css.png' alt="" width={'75'} height={'75'}/>
+            <h3>CSS</h3>
         </div>
 
 
         {/* tailwind */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/tailwind.png' alt="" width={'100'} height={'100'}/>
-            <h2>Tailwind</h2>
+            <Image src='/../public/tailwind.png' alt="" width={'75'} height={'75'}/>
+            <h3>Tailwind</h3>
         </div>
 
         
         {/* javascript */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/javascript.png' alt="" width={'100'} height={'100'}/>
-            <h2>Javascript</h2>
+            <Image src='/../public/javascript.png' alt="" width={'75'} height={'75'}/>
+            <h3>Javascript</h3>
         </div>
 
 
         {/* react */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/react.png' alt="" width={'100'} height={'100'}/>
-            <h2>React</h2>
+            <Image src='/../public/react.png' alt="" width={'75'} height={'75'}/>
+            <h3>React</h3>
         </div>
 
         {/* redux */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/redux.png' alt="" width={'100'} height={'100'}/>
-            <h2>Redux</h2>
+            <Image src='/../public/redux.png' alt="" width={'75'} height={'75'}/>
+            <h3>Redux</h3>
         </div>
 
         {/* next */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/nextjs.png' alt="" width={'100'} height={'100'}/>
-            <h2>Next</h2>
+            <Image src='/../public/nextjs.png' alt="" width={'75'} height={'75'}/>
+            <h3>Next</h3>
         </div>
 
         {/* github */}
         <div className='p-7 flex items-center justify-between rounded-xl shadow-lg shadow-shadowColor hover:scale-[1.1]'>
-            <Image src='/../public/github.png' alt="" width={'100'} height={'100'}/>
-            <h2>Github</h2>
+            <Image src='/../public/github.png' alt="" width={'75'} height={'75'}/>
+            <h3>Github</h3>
         </div>
 
       </div>
