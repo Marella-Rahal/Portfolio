8d9874e5ba93f8f8756ae2d4d34ec9a0d2d66d05
Marella-Rahal <marellarahhal20@gmail.com>
2022-10-14 14:32:17 +0300

    Portfolio

---
+++ components/ProjectItem.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+
+const ProjectItem = ({title,tech,img,url}) => {
+  return (
+    <div className='p-5 relative rounded-xl shadow-xl shadow-shadowColor group hover:bg-gradient-to-r from-gradientFrom to-gradientTo h-[500px]'>
+
+        <img className='h-[460px] w-full rounded-xl group-hover:opacity-5' src={img} alt=''/>
+
+        <div className='hidden group-hover:block text-bgColor text-center absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] space-y-5'>
+
+            <h3>{title}</h3>
+
+            <p>{tech}</p>
+
+            <div className='bg-bgColor text-textColor font-bold rounded-xl  cursor-pointer py-3 hover:scale-[1.1]'>
+                <Link href={url}>More Info</Link>
+            </div>
+            
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default ProjectItem
--- components/Projects.js
+++ components/Projects.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import ProjectItem from './ProjectItem'
 
 const Projects = () => {
   return (
-    <div>
+    <div className='px-10 pt-20 space-y-7'>
+
+      <p className='text-effectColor uppercase tracking-widest'>projects</p>
+
+      <h2>What I&apos;v Built</h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-7'>
+
+        <ProjectItem title='Shoppingo' tech='React' img='shoppingo.png' url='#' />
+        <ProjectItem title='Landing Page' tech='Next' img='landing-page.png' url='#' />
+       
+
+      </div>
       
     </div>
   )
