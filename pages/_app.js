159e1f552bd3b2af4273b154a1feb8c8dadc22b0
Marella <marellarahhal20@gmail.com>
2023-09-24 08:32:03 +0300

    adding icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,54 +1,57 @@
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import '../styles/globals.css'
-import {motion,AnimatePresence} from 'framer-motion';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
-const pageVariants={
-
-  hidden:{
-    opacity:0,
+const pageVariants = {
+  hidden: {
+    opacity: 0,
   },
-  visible:{
-    opacity:1
+  visible: {
+    opacity: 1,
   },
-  exit:{
-    opacity:0
-  }
+  exit: {
+    opacity: 0,
+  },
+};
 
-}
-
-
-function MyApp({ Component, pageProps}) {
-
-  const router=useRouter();
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
 
   return (
     <>
       <Head>
         <title>Portfolio | Marella Rahal</title>
         {/* <meta charset="UTF-8"/> */}
-        <meta name="description" content="Marella Rahal&apos;s Portfolio using Next.Js and Tailwind "/>
-        <meta name="keywords" content="React.Js , Next.Js, Tailwind, Css, javascript ,portfolio, Marella, Rahal "/>
-        <meta name="author" content="Marella Rahal"/>
+        <meta
+          name="description"
+          content="Marella Rahal's Portfolio using Next.Js and TailwindCSS "
+        />
+        <meta
+          name="keywords"
+          content="React.Js , Next.Js, TailwindCSS, Css, javascript ,portfolio, Marella, Rahal "
+        />
+        <meta name="author" content="Marella Rahal" />
+        <link rel="icon" href="logo.svg" />
         {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
       </Head>
 
-      <Navbar/>
+      <Navbar />
 
-      <AnimatePresence exitBeforeEnter>
-
-        <motion.div key={router.route} variants={pageVariants} initial="hidden" animate="visible" exit="exit">
-
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          variants={pageVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
           <Component {...pageProps} />
-
         </motion.div>
-        
       </AnimatePresence>
-
-      
     </>
-  ) 
+  );
 }
 
-export default MyApp
+export default MyApp;
