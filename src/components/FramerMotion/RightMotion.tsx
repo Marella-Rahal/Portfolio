e728a105a7a94e3814d2d350f8582c154cbae4fc
Marella <marellarahhal20@gmail.com>
2023-10-17 15:40:36 +0300

    some animation

---
+++ src/components/FramerMotion/RightMotion.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { motion } from "framer-motion";
+
+const RightMotion = ({ children }: motionType) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: "100%" }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.7 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default RightMotion;
